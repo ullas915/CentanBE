1d201317f1c1d8740abaff917fc1b265cccb09dd
ullas915 <hyynfnw@gmail.com>
2025-06-25 22:13:47 +0530

    Update SecurityConfig with new settings

--- src/main/java/com/example/UserSpecificChatBot/config/SecurityConfig.java
+++ src/main/java/com/example/UserSpecificChatBot/config/SecurityConfig.java
@@ -69,7 +69,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("https://centanfe.onrender.com"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
